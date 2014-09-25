60a7bd8158b890e934ccf1e696f52dd5ff0a42f8
Connor Humphries <tyedyehumps@gmail.com>
2014-09-25 16:59:34 -0400

    Added my Phonegap app for the availability report

---
+++ availability/avail-app/platforms/android/ant-build/classes.dex.d
@@ -0,0 +1,9 @@
+/home/connor/Stacker/Stacker-App/availability/avail-app/platforms/android/ant-build/classes.dex : \
+/home/connor/Stacker/Stacker-App/availability/avail-app/platforms/android/ant-build/classes/com/phonegap/helloworld/BuildConfig.class \
+/home/connor/Stacker/Stacker-App/availability/avail-app/platforms/android/ant-build/classes/com/phonegap/helloworld/R$drawable.class \
+/home/connor/Stacker/Stacker-App/availability/avail-app/platforms/android/ant-build/classes/com/phonegap/helloworld/R.class \
+/home/connor/Stacker/Stacker-App/availability/avail-app/platforms/android/ant-build/classes/com/phonegap/helloworld/HelloWorld.class \
+/home/connor/Stacker/Stacker-App/availability/avail-app/platforms/android/ant-build/classes/com/phonegap/helloworld/R$string.class \
+/home/connor/Stacker/Stacker-App/availability/avail-app/platforms/android/ant-build/classes/com/phonegap/helloworld/R$attr.class \
+/home/connor/Stacker/Stacker-App/availability/avail-app/platforms/android/ant-build/classes/com/phonegap/helloworld/R$xml.class \
+/home/connor/Stacker/Stacker-App/availability/avail-app/platforms/android/ant-build/dexedLibs/classes-9f8ec6d54473ab4e5b9ea9870cac005a.jar \
