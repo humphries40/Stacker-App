fed14fdd736e5f657863684884fe57f85c00d200
Connor Humphries <humphries.40@osu.edu>
2014-10-03 11:36:05 -0400

    add to avail js

--- availability/avail-app/www/js/avail.js
+++ availability/avail-app/www/js/avail.js
@@ -22,74 +22,61 @@ var availreport {
 	//events is an array of items various fields these are
 	//["Month", "day", "Year", "Starttime", "Endtime"]	
 
-	findOpen: function(events, filters) {
-		var events = new Array(
+	function findOpen(events, filters) {
+		events = new Array(
 			[7, 7, 2014, "1:00 PM", "2:00 PM"],
 			[7, 7, 2014, "5:00 PM", "6:00 PM"],
 			[7, 7, 2014, "8:00 PM", "9:00 PM"],
 			[7, 8, 2014, "10:00 AM", "2:00 PM"],
 			[7, 8, 2014, "4:00 PM", "5:00 PM"],
-			[7, 8, 2014, "7:00 PM", "11:00 PM"],
-			)
-		var filters = new Array(
+			[7, 8, 2014, "7:00 PM", "11:00 PM"]
+			);
+		filters = new Array(
 			[7, 7, 2014, "12:00 AM", "Start Day"],
-			[7, 8, 2014, "11:59 PM", "End Day"])
-		var freetime = new Array();
+			[7, 8, 2014, "11:59 PM", "End Day"]);
+		var freeTime = [];
 		var count = 0;
 		var curTime = filters[1];
+		curTime[5] = "Current Time";
 		var startFreeTime = filters[1];
 		//While events isn't empty or past our filter values we want to keep iterating
-		while(count < events.length && dayCompare(filters[1], events[count]) < 1 && dayCompare[filters[2], events[count] > -1])
-		{
-			//
+		while(count < events.length && compareDay(filters[1], events[count]) < 1 && compareDay(filters[2], events[count]) > -1)
+		{			
+			startFreeTime = curTime;
 			while(compareDay(curTime, events[count]) < 1)
 			{
-				startFreeTime = curTime;
-				var curTimeHour = curTime[4].substring(0,2);
-				var curTimeMinute = curTime[4].substring(3,5);
-				var curTimeAM = false;
-				if(curTime[4].substring(5,7) == "AM") {
-					curTimeAM = true;
-				}
-				curTimeMinute++;
-				if (curTimeMinute == 60) {
-					curTimeMinute == 0;
-					curTimeHour++;
-					if (curTimeHour == 12) {
-						curTimeAM = !curTimeAM;
-						if(curTimeAM == true)
-						{
-							curTime[2] = curTime[2]++;
-							if(curTime[2] == 29 && curTime[1] == 2 && curTime[3]%4 == 0 && (curTime%100 != 0 || (curTime%100 == 0 && curTime%400 == 0)))
-							{
-								curTime[2] = 1;
-								curTime[1] = 3;
-							}
-							//Need to add checks for other months
-							else if (curTime[2] = 31 && (curTime[1] )) {}
-							//need to add rollover for year
-
-						}
-
-					}
-				}
-			}
-			//add a free time event that start free time is startfreetime and end freetime is cur time
-			//then increment cur time to end of event
-			
-
+				curTime = incrementTime(curTime);
+			}			
+			//push event to freeTime
+			startFreeTime[5] = "Start Free Time";
+			freeTime.push(startFreeTime);
+			curTime[5] = "End Free Time";
+			freeTime.push(curTime);
+			//then increment curTime to end of event
+			curTime = events[count];
+			curTime[4] = curTime[5];
+			curTime[5] = "Current Time";
+			count++;
 		}
 		//add a check for curTime and add free time = time till filter end day
-
+		if(count > events.length && compareDay(curTime, filters[2]) < 1)
+		{
+			//push event to freeTime
+			startFreeTime = curTime;
+			startFreeTime[5] = "Start Free Time";
+			freeTime.push(startFreeTime);
+			curTime = filters[2];
+			curTime[5] = "End Free Time";
+			freeTime.push(curTime);
+		}
+		return freeTime;
 	}
-
 	//This returns -1 if dayOne is before dayTwo
-	// 0 if they are the same day
+	// 0 if they are the same time
 	// 1 if dayOne is after dayTwo
-
 	//dayOne is an element from the events array
-	compareDay: function(dayOne, dayTwo) {
-		dayCompare = -1
+	function compareDay(dayOne, dayTwo) {
+		var dayCompare = -1;
 		//compare years
 		if (dayOne[3] <= dayTwo[3]) {
 			//compare months
@@ -109,9 +96,9 @@ var availreport {
 						if(dayOne[4].substring(5,7) == "AM") {
 							dayTwoAM = true;
 						}		
-						if (((dayOneAM = true && dayTwoAM = false) || (dayOneAM == dayTwoAM))  && dayOneHour <= dayTwoHour && dayOneMinute <= dayTwoMinute) {
-							if(dayOneAM = dayTwoAM && dayOneHour == dayTwoHour && dayOneMinute == dayTwoMinute) {
-								dayCompare == 0;
+						if (((dayOneAM == true && dayTwoAM == false) || (dayOneAM == dayTwoAM))  && dayOneHour <= dayTwoHour && dayOneMinute <= dayTwoMinute) {
+							if(dayOneAM == dayTwoAM && dayOneHour == dayTwoHour && dayOneMinute == dayTwoMinute) {
+								dayCompare = 0;
 							}
 						}
 					}
@@ -128,9 +115,46 @@ var availreport {
 			dayCompare = 1;
 		}
 		return dayCompare;
-
 	}
-
-
-
+	
+	function incrementTime(curTime) 	{
+		var curTimeHour = curTime[4].substring(0,2);
+		var curTimeMinute = curTime[4].substring(3,5);
+		var curTimeAM = false;
+		if(curTime[4].substring(5,7) == "AM") {
+			curTimeAM = true;
+		}
+		//Incrementing curTime
+		curTimeMinute++;
+		minsOfFreeTime++;
+		if (curTimeMinute == 60) {
+			curTimeMinute = 0;
+			curTimeHour++;
+			if (curTimeHour == 12) {
+				curTimeAM = !curTimeAM;
+				if(curTimeAM){
+					curTime[2] = curTime[2]++;
+					//check for leap year
+					if(curTime[2] == 29 && curTime[1] == 2 && curTime[3]%4 == 0 && (curTime%100 != 0 || (curTime%100 == 0 && curTime%400 == 0))) {
+						curTime[2] = 1;
+						curTime[1] = 3;
+					}
+					else if (curTime[2] == 31 && (curTime[1] == (4 || 6 || 9 || 11)) ){
+						curtime[1]++;
+						curtime[2] = 1;
+					}
+					else if (curtime[2] == 32) {
+						curtime[1]++;
+						curtime[2] = 1;
+					}
+					if(curtime[1] == 13) {
+						curtime[1] = 1;
+						curtime[3]++;
+					}
+				}
+			}
+		}
+		curTime[4] = curTimeHour + ":" + curTimeMinute + " " + curTimeAM;
+		return curTime;
+	}
 }
