ebb3b892d52d060938aa980d2d5a00c91d8502cb
Dan Meehan <dan.meehan17@gmail.com>
2014-12-02 23:47:59 -0500

    Revert 783a293..521c039
    This rolls back to commit 783a2932181ffb0a0d7b9a5b196af5622c514de2.

--- test2/platforms/ios/www/js/jw-jqm-cal.js
+++ test2/platforms/ios/www/js/jw-jqm-cal.js
@@ -258,16 +258,7 @@
       
 	  
 	    $('.deleteevent').click(function(){
-		
-			$.post("http://server8.ies.cse.ohio-state.edu/stacker/delete_event.php",
-						{
-						sno:$(this).attr('cid')
-						},
-						function(data){
-						  var obj = $.parseJSON(data);
-						  alert(obj.err);				  
-						});
-		$.mobile.pageContainer.pagecontainer("change","#refreshcalendar", {transition: 'none',reloadPage: false});
+			alert("remove" + $(this).attr('cid'));
 		});
 	  
 	  });
@@ -284,6 +275,7 @@
 			$eventattr.attr('cid',event[plugin.settings.url]);
 			$eventattr.attr('class','deleteevent ui-btn ui-icon-delete ui-btn-icon-right');
 			$eventattr.appendTo($listItem);
+			$
          } else {
             $listItem.text( text );
          }
